c5b3c4992c042cd67819f3a2672d1b788a224170
Blair <blair.liikala@gmail.com>
2023-02-20 19:07:11 -0600

    Update addon.setup.php

--- addon.setup.php
+++ addon.setup.php
@@ -5,13 +5,13 @@
 return [
     'name'              => 'ee_captions',
     'description'       => 'Render captions in templates.',
-    'version'           => '1.1',
+    'version'           => '1.2',
     'author'            => 'Blair Liikala',
     'author_url'        => 'blairliikala.com',
     'namespace'         => 'BlairLiikala\EeCaptions',
     'settings_exist'    => false,
     'requires'       => [
         'php'   => '8.0',
-        'ee'    => '7.2.0'
+        'ee'    => '7.0.0'
     ],  
 ];
